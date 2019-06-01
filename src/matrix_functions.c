2c17d32148c1917a443e8e1e8178964a34c78829
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2019-06-01 01:10:01 -0300

    functions for matrices

--- src/matrix_functions.c
+++ src/matrix_functions.c
@@ -9,19 +9,40 @@
 
 
 // este main es solo para testear esto
+/*
 int main (void){
 	int m1[2][2] = {{2,2}, {2,2}};
 	int m2[2][2] = {{2,2}, {2,2}};
 	int ans[2][2];
 	printf("Sumamos\n");
 	add(2,2, m1, m2, ans);
-	printf("---------\n");
-	printf("quiero chequear. si da 4 esta ok. dio --> %d\n", ans[1][1]);
+	printMatrix(2, 2, ans);
 	printf("---------\n");
 	printf("Restamos\n");
 	substract(2,2, m1, m2, ans);
+	printMatrix(2, 2, ans);
+	printf("---------\n");
+	int t1[3][2] = {{1,2}, {3,4}, {5,6}};
+	int anst[2][3];
+	printf("Trasponemos\n");
+	transpose(3,2, t1, anst);
+	printMatrix(2, 3, anst);
+	printf("---------\n");
+	printf("Concatenamos\n");
+	int ansc[2][5];
+	concat(2, 2, 3, m2, anst, ansc);
+	printMatrix(2, 5, ansc);
+	printf("---------\n");
+	printf("Multiplicamos\n");
+	int e1[2][3] = {{1,2,3}, {4,5,6}};
+	int e2[3][1] = {{9},{8},{7}};
+	int multAnswer[2][1];
+	multiply(2,3,3,1, e1,e2,multAnswer);
+	printMatrix(2, 1, multAnswer);
+	printf("---------\n");
+	printf("---FIN---\n");
 }
-
+*/
 void add (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]){
 
 	int i;
@@ -32,10 +53,10 @@ void add (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][colum
 		for(j=0;j<columns;++j)
 		{
 			answer[i][j]=(m1[i][j]+m2[i][j])%251;
-			printf("%d ",answer[i][j]);
+			//printf("%d ",answer[i][j]);
 		}
 		
-		printf("\n");
+		//printf("\n");
 	}
 
 }
@@ -53,22 +74,73 @@ void substract (size_t rows, size_t columns, int m1[rows][columns], int m2[rows]
 			if(answer[i][j] < 0){
 				answer[i][j] = answer[i][j] + 251;
 			}
-			printf("%d ",answer[i][j]);
+			//printf("%d ",answer[i][j]);
 		}
 		
-		printf("\n");
+		//printf("\n");
 	}
 
 }
 
-void transpose (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]){
+void transpose (size_t rows, size_t columns, int m[rows][columns],  int answer[columns][rows]){
+
+	int i;
+	int j;
+
+	for (i = 0; i < rows; i++){
+        for (j = 0; j < columns; j++){
+            answer[j][i] = m[i][j];
+        }
+	}
+}
+
+void multiply (size_t rows1, size_t columns1, size_t rows2, size_t columns2 ,int m1[rows1][columns1], int m2[rows2][columns2], int answer[rows1][columns2]){
+	//columns1 y rows2 deben ser iguales!
+	if(columns1 != rows2){
+		printf("Invalid multiplication.\n");
+		return;
+	}
+
+	int i;
+	int j;
+	int k;
+	int sum = 0;
+	for (i = 0; i < rows1; i++) {
+      for (j = 0; j < columns2; j++) {
+        for (k = 0; k < rows2; k++) {
+          	sum = sum + m1[i][k]*m2[k][j];
+        }
+        answer[i][j] = sum;
+        sum = 0;
+      }
+    }
 	
 }
 
-void multiply (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]){
-	
+void concat (size_t rows, size_t columns1,size_t columns2, int m1[rows][columns1], int m2[rows][columns2], int answer[rows][columns1+columns2]){
+	int i;
+	int j;
+	int aux;
+
+	for(i=0;i<rows;i++){
+		for(j=0; j<columns1;j++){
+			answer[i][j] = m1[i][j];
+		}
+		aux = j;
+		for(j=0; j<columns2;j++){
+			answer[i][aux] = m2[i][j];
+			aux++;
+		}
+	}
 }
 
-void concat (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]){
-
+void printMatrix(size_t rows, size_t columns, int m[rows][columns]){
+	int i;
+	int j;
+	for (i=0; i<rows; i++){
+		    for(j=0; j<columns; j++){
+		        printf("%d  ", m[i][j]);
+		    }
+		    printf("\n");
+	}
 }
--- src/matrix_functions.h
+++ src/matrix_functions.h
@@ -3,7 +3,8 @@
 
 void add (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]);
 void substract (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]);
-void transpose (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]);
-void multiply (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]);
-void concat (size_t rows, size_t columns, int m1[rows][columns], int m2[rows][columns], int answer[rows][columns]);
+void transpose (size_t rows, size_t columns, int m[rows][columns],  int answer[columns][rows]);
+void multiply (size_t rows1, size_t columns1, size_t rows2, size_t columns2 ,int m1[rows1][columns1], int m2[rows2][columns2], int answer[rows1][columns2]);
+void concat (size_t rows, size_t columns1,size_t columns2, int m1[rows][columns1], int m2[rows][columns2], int answer[rows][columns1+columns2]);
+void printMatrix(size_t rows, size_t columns, int m[rows][columns]);
 #endif
