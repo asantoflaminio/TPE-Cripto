aff882df1382f94833062da712c9d342f3acaa8a
Alejandro <alejandro@localhost.localdomain>
2019-06-17 17:26:46 -0300

    more cleanup2

--- src/utils.c
+++ src/utils.c
@@ -22,18 +22,3 @@ const char *get_filename_ext(const char *filename) {
     return dot + 1;
 }
 
-void assure(int truth, char* message) {
-  if (!truth) {
-    printf("%s", message);
-    exit(EXIT_FAILURE);
-  }
-}
-/*
-int inverse(int i) {
-
-  i %= 251;
-  if (i < 1 || i > 251) {
-    return -1;
-  }
-  return inverses[i - 1];
-}*/
